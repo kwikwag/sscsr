3d12dc7cf834679defd74f8060947a802fc7499f
Yuval Sedan <yuval.sedan@mail.huji.ac.il>
2017-05-26 11:12:58 +0300

    Break up server code to methods

--- server.rb
+++ server.rb
@@ -1,8 +1,21 @@
 require "socket"
-server = TCPServer.open(2626)
-loop do
-	Thread.fork(server.accept) do |client| 
-		client.puts("Hello, I'm Ruby TCP server", "I'm disconnecting, bye :*")
-		client.close
+
+def handle_client_session(client)
+	client.puts "Hello, world."
+end
+
+def threaded_server(port)
+	server = TCPServer.open(port)
+	loop do
+		Thread.fork(server.accept) do |client|
+			yield client
+			client.close
+		end
+	end
+end
+
+if __FILE__ == $0
+	threaded_server(2626) do |client|
+		handle_client_session(client)
 	end
 end
