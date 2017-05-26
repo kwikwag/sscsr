a2fed8b8477e4da1fb8c44a4a720378b681792f9
Yuval Sedan <yuval.sedan@mail.huji.ac.il>
2017-05-26 15:07:51 +0300

    Complete with symmetric and asymmetric encryptions, and with command-line arguments

--- client.rb
+++ client.rb
@@ -1,16 +1,63 @@
-require "socket"
-def get_data()
-	File.open('/dev/urandom', 'r') do |file|
-		buffer = file.read(1024)
-		yield buffer.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => ' ')
+require 'socket'
+require 'openssl'
+require 'json'
+require 'securerandom'
+require 'base64'
+require 'optparse'
+
+def gen_random_bytes(len, insist_on_urandom=true)
+	if insist_on_urandom
+		File.open( '/dev/urandom', 'r') do |file|
+			return file.read(len)
+		end
+	else
+		return SecureRandom.random_bytes(len)
 	end
+end
+		
+def get_data()
+	buffer = gen_random_bytes(1024, insist_on_urandom=((ENV['SSCSR_INSIST_ON_URANDOM'] || '1') == '1'))
+	return buffer.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => ' ')
+end
+
+def sym_encrypt_data(data, key)
+	cipher = OpenSSL::Cipher::AES.new(128, :CFB).encrypt
+	iv = cipher.random_iv
+	cipher.key = key
+	cipher.iv = iv
+	# TODO : encrypt iv with key using iv-less cipher
+	return { :iv => Base64.encode64(iv), :data => Base64.encode64(cipher.update(data) + cipher.final) }
+end
+
+def asym_encrypt_data(data, key)
+	public_key = OpenSSL::PKey::RSA.new(key)
+	return { :data => Base64.encode64(public_key.public_encrypt(data)) }
+end
+
+def fail_usage()
+	STDERR.puts <<~USAGE
+		Usage: SSCSR_KEY=key [SSCSR_INSIST_ON_URANDOM={0,1}] ruby #{__FILE__} HOST PORT {symmetric,asymmetric}"
+	USAGE
+	exit(1)
 end
 
 if __FILE__ == $0
-	TCPSocket.open("localhost", 2626) do |sock|
-		get_data do |data|
-			sock.send(data, 0)
+	key = ENV['SSCSR_KEY'] || 'using a hard-coded default key is not very secure, but it will do.'
+
+	if ARGV.length != 3
+		fail_usage
+	end
+	host = ARGV[0]
+	port = ARGV[1].to_i
+	symmetric = ARGV[2] == 'symmetric' # really, i should check this argument
+
+	TCPSocket.open(host, port) do |sock|
+		if symmetric
+			encrypted_obj = sym_encrypt_data(get_data(), key)
+		else
+			encrypted_obj = asym_encrypt_data(get_data(), key)
 		end
+		sock.send(JSON.generate(encrypted_obj), 0)
 		sock.close
 	end
 end
--- server.rb
+++ server.rb
@@ -1,40 +1,59 @@
 require 'socket'
 require 'logger'
+require 'openssl'
+require 'json'
+require 'base64'
+require 'pp'
 
 $logger = Logger.new(STDOUT)
-$logger.level = Logger::DEBUG
+$logger.level = Logger::WARN if ENV['DEBUG'].nil?
 
 BUFFER_SIZE = 65536
 
 class NoData < StandardError
 end
 
-def handle_client_session(client)
-	$logger.debug('Session start.')
-	buffer = client.read(65536)
-	$logger.debug("Read #{buffer.length} bytes.")
-=begin
-	disconnected = false
-	while not disconnected do
-		begin
-			$logger.debug('Before non-block read')
-			data = client.recv_nonblock(BUFFER_SIZE)
-			disconnected = data.length == 0
-			$logger.debug('After non-block read')
-		rescue IO::WaitReadable, NoData
-			$logger.debug('Before select')
-			result = IO.select([client], nil, nil, 1)
-			disconnected = result.nil?
-			$logger.debug("After select... Disconnected? #{disconnected}")
-			retry unless disconnected
-		end
-	
-		disconnected = true if c > 5
-		$logger.debug("Received #{data.length} bytes")
-	end
-=end
-	$logger.debug('Client session ended.')
+class InvalidEncryptionTag < StandardError
 end
+
+def handle_client_session(client, key, password, symmetric)
+	$logger.debug('Session start.')
+
+	# don't expect a too-long message
+	buffer = client.read(65536)
+	raise NoData.new if buffer.nil?
+	$logger.debug("Read #{buffer.length} bytes.")
+
+	# read message body and let JSON module
+	# handle any errors
+	encrypted_obj = JSON.parse(buffer)
+
+	# decrypt and display the message
+	$logger.debug("JSON read: #{JSON.generate(encrypted_obj)}. Decrypting.")
+	if symmetric
+		if not password.nil?
+			$logger.warn('Password not nil for symmetric encryption. Ignoring.')
+		end
+		print sym_decrypt_data(encrypted_obj, key)
+	else
+		print asym_decrypt_data(encrypted_obj, key, password)
+	end
+end
+
+def asym_decrypt_data(obj, key, password)
+	# see https://stackoverflow.com/questions/12662902/ruby-openssl-asymmetric-encryption-using-two-key-pairs
+	private_key = OpenSSL::PKey::RSA.new(key, password)
+	data = private_key.private_decrypt(Base64.decode64(obj['data']))
+end
+
+def sym_decrypt_data(obj, key)
+	# see http://ruby-doc.org/stdlib-2.4.1/libdoc/openssl/rdoc/OpenSSL/Cipher.html
+	decipher = OpenSSL::Cipher::AES.new(128, :CFB).decrypt
+	decipher.key = key
+	decipher.iv = Base64.decode64(obj['iv'])
+	return decipher.update(Base64.decode64(obj['data'])) + decipher.final
+end
+
 
 def threaded_server(port)
 	server = TCPServer.open(port)
@@ -53,8 +72,38 @@ def threaded_server(port)
 	end
 end
 
+def fail_usage()
+	STDERR.puts <<~USAGE
+		Usage: env SSCSR_KEY=key [SSCSR_PRIVATE_KEY_PASSWORD=pwd] ruby #{__FILE__} PORT {symmetric,assymetric}
+
+		Note: SSCSR_PRIVATE_KEY_PASSWORD is required if using asymmetric encryption.
+
+		Examples:
+			1. SSCSR_KEY=$(seq 1 10 | tr '\\n' 'x') ruby #{__FILE__} 2626 symmetric
+			2. SSCSR_KEY=$(cat private_key.pem) SSCSR_PRIVATE_KEY_PASSWORD=helloworld ruby #{__FILE__} 2626 asymmetric
+	USAGE
+	exit(1)
+end
+
+
 if __FILE__ == $0
-	threaded_server(2626) do |client|
-		handle_client_session(client)
+	key = ENV['SSCSR_KEY']
+
+	if (ARGV.length != 2 or key.nil?)
+		fail_usage
+	end
+
+	port = ARGV[0].to_i
+	symmetric = ARGV[1] == 'symmetric' # really, i should check this argument
+
+	if not symmetric
+		password = ENV['SSCSR_PRIVATE_KEY_PASSWORD']
+		if password.nil?
+			fail_usage
+		end
+	end
+
+	threaded_server(port) do |client|
+		handle_client_session(client, key, password, symmetric)
 	end
 end
